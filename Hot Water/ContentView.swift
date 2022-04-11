ccd6ae1a8077a2b1cb7ef37be5db19bf23f3b2d3
DmitrySokoltsov <86969547+dmitrysokoltsov@users.noreply.github.com>
2022-04-11 18:06:22 +0300

    Initial Commit

---
+++ Hot Water/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hot Water
+//
+//  Created by Dmitry Sokoltsov on 11.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hot Water/Hot_WaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_WaterApp.swift
+//  Hot Water
+//
+//  Created by Dmitry Sokoltsov on 11.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Hot_WaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
